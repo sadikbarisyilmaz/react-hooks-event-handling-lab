9cf2c29a2b14bc0f916b689fde4cbe49935a8a14
Sadık Barış Yılmaz <sadikbarisyilmaz@gmail.com>
2023-06-11 18:15:53 +0300

    completed assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+export default function EyesOnMe() {
+  return (
+    <div>
+      <button
+        onFocus={() => console.log("Good!")}
+        onBlur={() => console.log("Hey! Eyes on me!")}
+      >
+        Eyes On Me
+      </button>
+    </div>
+  );
+}
